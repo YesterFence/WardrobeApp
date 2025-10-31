357b6e5f67be7c9be937f09023f2196775fc812e
YesterFence <ryantatsuyalee@gmail.com>
2025-10-30 19:26:20 -0600

    Wk07 pt2

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -465,25 +465,25 @@ const styles = StyleSheet.create({
 //   );
 // }
 
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: '#25292e',
-// //     alignItems: 'center',
-// //   },
-// //   imageContainer: {
-// //     flex: 1,
-// //   },
-// //   footerContainer: {
-// //     flex: 1 / 3,
-// //     alignItems: 'center',
-// //   },
-// //   optionsContainer: {
-// //     position: 'absolute',
-// //     bottom: 80,
-// //   },
-// //   optionsRow: {
-// //     alignItems: 'center',
-// //     flexDirection: 'row',
-// //   },
-// // });
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//   },
+//   footerContainer: {
+//     flex: 1 / 3,
+//     alignItems: 'center',
+//   },
+//   optionsContainer: {
+//     position: 'absolute',
+//     bottom: 80,
+//   },
+//   optionsRow: {
+//     alignItems: 'center',
+//     flexDirection: 'row',
+//   },
+// });
